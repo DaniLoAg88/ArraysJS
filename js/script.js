47603296c6df849d708d1f4f76d82140137ad5d4
DaniLoAg88 <danilopezag@gmail.com>
2024-06-06 19:54:22 +0200

    Tercer commit

--- js/script.js
+++ js/script.js
@@ -63,3 +63,55 @@ for(let i = 0; i < 5; i++){
 }
 
 console.log(notas);
+
+
+/**
+ * ACTIVIDAD 3.2
+ * Crear un cuadro div y un desplegable indicando los colores, y según la elección cambiar su color
+ */
+
+
+let coloresRGB = new Array();
+coloresRGB[0] = "#F39C12";
+coloresRGB[1] = "#C0F312";
+coloresRGB[2] = "#12F3E5";
+coloresRGB[3] = "#F312AF";
+coloresRGB[4] = "#F31212";
+
+let nombreColores = ["Naranja", "Lima", "Turquesa", "Rosa", "Rojo"];
+
+//Las dos formas son válidas para declarar e inicilizar un array
+
+
+let colores = document.querySelector("#colores"); //Trae el select (con # por ID)
+
+for (let i = 0; i < nombreColores.length; i++) {
+    let opcion = document.createElement("option"); //Creamos un option para el select que tenemos en HTML(crea una etiqueta)
+    opcion.textContent = nombreColores[i]; //Metemos el texto del option (nombre del color)
+    opcion.setAttribute("value",i); //Le agregamos como atributo el value con el valor de i
+    colores.appendChild(opcion); /*Le creamos un hijo al select que hemos recogido en éste objeto colores
+    y se lo añadimos con todos los valores que le hemos dado al objeto opcion*/
+}
+
+
+let contenedor = document.querySelector(".container"); //Recogemos el contenedor que creamos en HTML (con . porque es por clase)
+
+/*Ahora vamos a decirle al objeto colores que es el select que hemos recogido del HTML que escuche
+    si hay algún cambio en la selección*/
+colores.addEventListener("change", function(){
+
+    if(colores.value == ""){
+        contenedor.style.backgroundColor = "black"; //Si selecciona el valor vacío el fondo vuelve a negro
+    } else{
+        contenedor.style.backgroundColor = coloresRGB[colores.value];/* Como siempre está
+        a la escucha de un cambio, cuando cambia recoge el valor seleccionado y se lo mete
+        al color de fondo del contenedor*/
+    }
+    
+});
+
+
+
+
+
+
